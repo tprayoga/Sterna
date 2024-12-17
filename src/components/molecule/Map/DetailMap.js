b3d5f898ad7debc0a59fbf85b3e56f26c3da820a
tprayoga <tprayogaa26@gmail.com>
2024-12-17 12:51:02 +0700

    map

--- src/components/molecule/Map/DetailMap.js
+++ src/components/molecule/Map/DetailMap.js
@@ -1,19 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
-import {
-  MapContainer,
-  TileLayer,
-  GeoJSON,
-  Marker,
-  Popup,
-  useMapEvent,
-  useMap,
-  useMapEvents,
-  ImageOverlay,
-} from "react-leaflet";
-import {
-  setLocationFilter,
-  setRealCoordinate,
-} from "@redux/features/location/locationSlice";
+import { MapContainer, TileLayer, GeoJSON, Marker, Popup, useMapEvent, useMap, useMapEvents, ImageOverlay } from "react-leaflet";
+import { setLocationFilter, setRealCoordinate } from "@redux/features/location/locationSlice";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import * as ELG from "esri-leaflet-geocoder";
@@ -25,9 +12,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { setLonLat } from "@redux/features/location/locationSlice";
 
 const DetailMap = ({ zoom, center, data }) => {
-  const { lonLat, location, locationFilter } = useSelector(
-    (state) => state.location
-  );
+  const { lonLat, location, locationFilter } = useSelector((state) => state.location);
   const dispatch = useDispatch();
 
   const iconPerson = new L.Icon({
@@ -45,19 +30,13 @@ const DetailMap = ({ zoom, center, data }) => {
   // ];
 
   const bounds = [
-    [-11.2585669, 94.6530261576],
-    [6.4582086834, 141.45385176],
+    [-11.7685669, 94.3630261576],
+    [7.1982087834, 141.52185176],
   ];
 
   return (
     <>
-      <MapContainer
-        center={center}
-        zoom={zoom}
-        style={{ width: "100%", height: "100%", zIndex: 0 }}
-        minZoom={5}
-        attributionControl={false}
-      >
+      <MapContainer center={center} zoom={zoom} style={{ width: "100%", height: "100%", zIndex: 0 }} minZoom={5} attributionControl={false}>
         <TileLayer
           // url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=635oHCTIGdZ2lX8MtqJk"
           url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
